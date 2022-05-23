be6063b6f26eee923b00d719fcde44f430bfae4d
JeffRisberg <j.s.risberg@gmail.com>
2022-05-22 23:40:58 -0700

    added reducers

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const ActionTypes = {
+   SET_USER: 'SET_USER',
+   REGISTER_USER: 'REGISTER_USER',
+   CLEAR_USER: 'CLEAR_USER',
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { createBrowserHistory } from 'history';
+
 import './index.css';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import configureStore from './store';
 
+const history = createBrowserHistory({basename: '/'});
+
+const store = configureStore({initialState: {}, history});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app-root')
+);
